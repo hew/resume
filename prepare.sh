0df6521f72f6e6c114860ec8b02d8fd95a901908
Matthew Jones <matt@klue.com>
2018-05-08 21:40:32 -0700

    mods script, adds ignore.

--- prepare.sh
+++ prepare.sh
@@ -1,4 +1,4 @@
 echo preparing...
-hackmyresume build resume.json TO build/index.html build/index.md build/index.txt -t ../basis/
+hackmyresume build resume.json TO build/index.html build/index.md build/index.txt -t ./fresh-resume-theme/
 cp build/index.html index.html
 echo done.
