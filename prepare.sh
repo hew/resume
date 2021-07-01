6f30f28b71b8236d7e1180fd3d69dd3b65ab91dd
Matthew Jones <matt@hew.tools>
2021-06-30 17:06:48 -0700

    feat: update

--- prepare.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo preparing...
-hackmyresume build resume.json TO build/index.html build/index.md build/index.txt -t ./fresh-resume-theme/
-cp build/index.html index.html
-echo done.
